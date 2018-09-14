5e8a5ed99a361a90dafe079282fc96a314172a15
Mohammed Ameen <jegroamin@gmail.com>
2018-09-14 19:36:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function getIssues() {
+  fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(resp => resp.json())
+  .then(json => console.log(json));
 }
 
 function showIssues(json) {
@@ -8,11 +11,22 @@ function createIssue() {
 }
 
 function showResults(json) {
+  console.log(`<a href="${json.html_url}"> </a>`)
+   $('#results').append(link)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  
+  
+ 
+ const token = '81c96bf385beafd7530f3b3cca18fd3c30e4fc57';
+ 
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
